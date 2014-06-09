f4794da8cc167e88db5d707bb7775b7732fcd331
Szépe Viktor <viktor@szepe.net>
2014-06-10 00:25:42 +0200

    Create wp-config.php

---
+++ templates/wp-config.php
@@ -0,0 +1,14 @@
+<?php
+
+// DB crendtials
+// this files looks like a real wp-config.php
+
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+$table_prefix = 'wp_';
+define('WPLANG', 'hu_HU');
